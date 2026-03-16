35888c14616586c3e3deb67f5b76ff3dac9437bd
ursulinastarry <paulinawambua8@gmail.com>
2026-03-16 07:53:00 +0300

    vercel home

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html')
+      }
+    }
   },
   server: {
     open: true
